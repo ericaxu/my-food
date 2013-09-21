83c3e90de4cb89fcad50441dc404c542201c1209
Erica Xu <erica.xu@uwaterloo.ca>
2013-09-20 21:51:31 -0400

    Fixed menu styles and icons.

--- js/ui.js
+++ js/ui.js
@@ -20,18 +20,28 @@ function detectDragAndDrop () {
 	});
 }
 
+function expandPanel() {
+	console.log("expandPanel");//t
+	var documentHeight = $(document).height();
+	console.log(documentHeight);//t
+	$(".slide-menu, .ui-panel-inner").height(documentHeight);
+	console.log($(".ui-panel-inner").height());//t
+}
+
 detectDragAndDrop();
 
 $(document).on("submit", "form", function(e) {
-	var currentPage = $.mobile.activePage.attr('id')
+	var currentPage = $.mobile.activePage.attr("id")
 	$("#" + currentPage).find(".submit").click();
 	e.preventDefault();
 	e.stopImmediatePropagation();
-}).on("click", ".link", function() {
-	
 }).bind("mobileinit", function () {
-    $.mobile.ajaxEnabled = false;
-}).ready(function() {
-	var documentHeight = $(document).height();
-	$(".slide-menu, .ui-panel-inner").height(documentHeight);
-});
+	$.mobile.ajaxEnabled = false;
+	expandPanel();
+}).on("click", ".link", function() {
+	var newPage = $(this).attr("class").split(" ")[1];
+	$.mobile.changePage("#"+newPage, {
+		transition: "slide"
+	});
+	expandPanel();
+}).on("pageinit", "*", expandPanel);
